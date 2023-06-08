cc1b2371893f5f46b91418bd4171608415956b23
EliedersonLinhares <eliederson250@gmail.com>
2023-06-08 12:26:35 -0300

    Using ProblemDetails in exceptions

--- src/main/java/com/esl/springbootlogin/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/esl/springbootlogin/security/jwt/AuthEntryPointJwt.java
@@ -1,15 +1,13 @@
 package com.esl.springbootlogin.security.jwt;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
@@ -24,7 +22,11 @@ import com.esl.springbootlogin.security.jwt.exception.validators.StandardError;
 import com.esl.springbootlogin.security.jwt.exception.validators.ValidationError;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-//Criamos a classe uthEntryPointJwt que implementa AuthenticationEntryPoint
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//Criamos a classe AuthEntryPointJwt que implementa AuthenticationEntryPoint
 //sobrescrevendo o método commence. Esse método irá se ativar em qualquer
 //momento que um usuario não autenciado requisitar um recurso http e um
 //AuthenticationException será ativado 
@@ -39,15 +41,32 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-        final Map<String, Object> body = new HashMap<>();
-
-        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-        body.put("error", "Acesso não authorizado");
-        body.put("message", "Usuário ou senha inválidos");
-        body.put("path", request.getServletPath());
+        /*
+         * final Map<String, Object> body = new HashMap<>();
+         * 
+         * body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+         * body.put("error", "Acesso não authorizado");
+         * body.put("message", "Usuário ou senha inválidos");
+         * body.put("path", request.getServletPath());
+         * 
+         */
+        ProblemDetail problemDetail = details(request, "Acesso não authorizado", "Usuário ou senha inválidos");
 
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), body);
+        mapper.writeValue(response.getOutputStream(), problemDetail);
+
+    }
+
+    private ProblemDetail details(HttpServletRequest request, String title, String detail) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        problemDetail.setTitle(title);
+        problemDetail.setDetail(detail);
+        problemDetail.setType(URI.create(request.getServletPath()));
+        problemDetail.setProperty("TimeStamp", dtf.format(now));
+        return problemDetail;
     }
 
     @ExceptionHandler(value = { AccessDeniedException.class })
@@ -57,15 +76,10 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-        final Map<String, Object> body = new HashMap<>();
-
-        body.put("status", HttpServletResponse.SC_FORBIDDEN);
-        body.put("error", "Acesso Negado");
-        body.put("message", "Usuario sem permissões nescessarias");
-        body.put("path", request.getServletPath());
+        ProblemDetail problemDetail = details(request, "Acesso negado", "Usuário sem permissões necessárias");
 
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), body);
+        mapper.writeValue(response.getOutputStream(), problemDetail);
     }
 
     // Tratar erros de validação
@@ -86,30 +100,5 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
 
     }
-
-    /*
-     * @ExceptionHandler(value = { InternalServerError.class })
-     * public void commence(HttpServletRequest request, HttpServletResponse
-     * response,
-     * InternalServerError internalServerError) throws IOException {
-     * // logger.error("AccessDenied error: {}",
-     * accessDeniedException.getMessage());
-     * // httpServletResponse.setStatus(HttpStatus.FORBIDDEN.value());
-     * // httpServletResponse.getWriter().write(convertObjectToJson(new
-     * // ErrorResponse(ResponseMessages.NOT_PERMITTED)));
-     * response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-     * response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-     * 
-     * final Map<String, Object> body = new HashMap<>();
-     * 
-     * body.put("status", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-     * body.put("error", "InternalServerError");
-     * body.put("message", "Erro no servidor de dados");
-     * body.put("path", request.getServletPath());
-     * 
-     * final ObjectMapper mapper = new ObjectMapper();
-     * mapper.writeValue(response.getOutputStream(), body);
-     * }
-     */
 
 }
